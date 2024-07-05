660dce6f4a579c29b862e489c389b41813a58290
Jaarabytes <xavierandole78@gmail.com>
2024-07-05 13:24:17 +0300

    fix: big problem here
    Turns out you are not  supposed to `{export: image}` if you are using
    nextjs `Image`, I've somehow fixed it but i'm not sure of the aftermath.
    However, `npm run dev` finally works. This has taken like 2 and a half
    hours, ukijiskia MPESA number ni io hio

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   // add images to next
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "picsum.photos",
+  //       port: "",
+  //     },
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
